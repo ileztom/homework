7e4c159f31cea2a26cb8edbc6eacb7d42ffb991a
ileztom <ileztom@mail.ru>
2021-10-21 17:10:13 +0300

    Initial commit

---
+++ src/main/java/lesson3/lesson3.java
@@ -0,0 +1,58 @@
+package lesson3;
+
+public class lesson3 {
+    public static void main(String[] args){
+        System.out.println(ex1(20,10));
+        ex2(5);
+        System.out.println(ex3(3));
+        ex4(5,6);
+        System.out.println(ex5(2021));
+    }
+
+
+    public static boolean ex1(int a,int b){
+        int sum = a + b;
+        if(sum >= 10){
+            if (sum <= 20)
+                return true;
+        }
+        else{
+            return false;
+        }
+        return false;
+    }
+
+    public static void ex2(int a){
+        if(a >= 0){
+            System.out.println("положительное");
+        }
+        else{
+            System.out.println("отрицательное");
+        }
+    }
+
+    public static boolean ex3(int a){
+        if (a < 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static void ex4(int a, int b){
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print("_");
+            }
+            System.out.println();
+
+        }
+    }
+    public static boolean ex5(int a){
+        if (a % 4 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
